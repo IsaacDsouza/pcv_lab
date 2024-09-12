20c26b87109d09897a3872dff4c5d1ab75fe4301
IsaacDsouza <isaacmdsouza@gmail.com>
2024-09-12 11:31:48 +0530

    Scaling an image

---
+++ scaling.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2 as cv
+
+img = cv.imread(r'/home/isaac/Desktop/pcv/pcv_lab/images.jpeg', 0)
+
+rows, cols = img.shape
+
+img_shrinked = cv.resize(img, (250, 200), interpolation=cv.INTER_AREA)
+
+cv.imshow('shrinked', img_shrinked)
+
+img_enlarged = cv.resize(img_shrinked, None, fx=1.5, fy=1.5, interpolation = cv.INTER_CUBIC)
+
+cv.imshow('enlarged', img_enlarged)
+
+cv.waitKey(0)
+
+cv.destroyAllWindows()
+
+
